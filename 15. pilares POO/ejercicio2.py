795ae0b502c5e4ff65603f86c1015af517d94ec6
Jhon Chavez <jhonf.6520@gmail.com>
2025-03-06 23:42:53 -0500

    Terminando python

--- 15. pilares POO/ejercicio2.py
+++ 15. pilares POO/ejercicio2.py
@@ -1,55 +1,82 @@
+
 '''
-Realizar un programa en el cual se declaren dos valores enteros por teclado utilizando el método __init__. 
-Calcular después la suma, resta, multiplicación y división. Utilizar un método para cada una e imprimir los resultados obtenidos.
-Llamar a la clase Calculadora.
+    Realizar un programa en el cual se declaren dos valores enteros por teclado utilizando el método __init__. 
+    Calcular después la suma, resta, multiplicación y división. Utilizar un método para cada una e imprimir los resultados obtenidos.
+    Llamar a la clase Calculadora.
+    
+    class Calculadora():
+        def __init__(self, valor1, valor2):
+            self.valor1 = valor1
+            self.valor2 = valor2
+
+        def operaciones(self, suma, resta, division, multiplicacion):
+            self.suma = valor1 + valor2
+            self.resta = valor1 - valor2
+            self.division = valor1 / valor2
+            self.multiplicacion = valor1 * valor2
+
+            return suma, resta, multiplicacion, division
+            
+    
+        print("El resultado de la suma es: ", suma)
+        print("El resultado de la resta es: ", resta)
+        print("El resultado de la multiplicacion es: ", multiplicacion)
+        print("el resultado de la division es: ", division)
+
+            
+
+        def resta(self):
+            resta = self.valor1 + self.valor2
+            print("El resultado de la resta es: ", resta)
+
+        def multiplicacion(self):
+            multiplicacion = self.valor1 * self.valor2
+            print("El resultado de la multiplicacion es: ", multiplicacion)
+
+        def division(self):
+            division = self.valor1 / self.valor2 
+            print("el resultado de la division es: ", division)
+ 
+
+    valor1 = int(input("Por favor ingrese el primer valor: "))
+    valor2 = int(input("Por favor ingrese el segundo valor: "))
+    calculadora = Calculadora(valor1, valor2)
+    calculadora.operaciones()
+
+
+ 
+    calculadora.suma()
+    calculadora.resta()
+    calculadora.multiplicacion()
+    calculadora.division()
+
 '''
+
+# solución del curso
 
 class Calculadora():
-    def __init__(self, valor1, valor2):
-        self.valor1 = valor1
-        self.valor2 = valor2
+     def __init__(self):
+          self.num1 = int(int(input("Por favor ingrese un número: ")))
+          self.num2 = int(int(input("Por favor ingrese un número: ")))
 
-    def operaciones(self, suma, resta, division, multiplicacion):
-        self.suma = valor1 + valor2
-        self.resta = valor1 - valor2
-        self.division = valor1 / valor2
-        self.multiplicacion = valor1 * valor2
-
-        return suma, resta, multiplicacion, division
-        
-        
-'''     print("El resultado de la suma es: ", suma)
-        print("El resultado de la resta es: ", resta)
-        print("El resultado de la multiplicacion es: ", multiplicacion)
-        print("el resultado de la division es: ", division)
-'''
-        
-'''
-    def resta(self):
-        resta = self.valor1 + self.valor2
-        print("El resultado de la resta es: ", resta)
-
-    def multiplicacion(self):
-        multiplicacion = self.valor1 * self.valor2
-        print("El resultado de la multiplicacion es: ", multiplicacion)
-
-    def division(self):
-        division = self.valor1 / self.valor2 
-        print("el resultado de la division es: ", division)
-'''
-
-valor1 = int(input("Por favor ingrese el primer valor: "))
-valor2 = int(input("Por favor ingrese el segundo valor: "))
-calculadora = Calculadora(valor1, valor2)
-calculadora.operaciones()
-
-
-'''
+     def suma(self):
+          self.suma = self.num1 + self.num2
+          print("El resultado de la suma es: ", self.suma)
+     
+     def resta(self):
+          self.resta = self.num1 - self.num2
+          print("El resultado de la resta es: ", self.resta)
+     
+     def multiplicacion(self):
+          self.multiplicacion = self.num1 * self.num2
+          print("El resultado de la multipliciación es: ", self.multiplicacion)
+     
+     def division(self):
+          self.division = self.num1 / self.num2
+          print("El resultado de la división es: ", self.division)
+     
+calculadora = Calculadora()
 calculadora.suma()
 calculadora.resta()
 calculadora.multiplicacion()
 calculadora.division()
-'''
-
-
-
--- 15. pilares POO/ejercicio3.py
+++ 15. pilares POO/ejercicio3.py
@@ -11,19 +11,25 @@ class Fabrica():
         self.precio = precio
 
 class Moto(Fabrica):
-    def str(self):
-        return "el número de llantas son: ", self.llantas, "\ncolor: ", self.color, "\nPrecio: ", self.precio
+    def datos(self):
+        print("el número de llantas de la moto son", self.llantas) 
+        print("El color de la moto es", self.color)
+        print("El precio de la moto es $",self.precio)
 
 class Carro(Fabrica):
-    def str(self):
-        return "El número de llantas son: ", self.llantas, "\ncolor: ", self.color, "\nPrecio: ", self.precio
+    def datos(self):
+        print("El número de llantas del carro son", self.llantas) 
+        print("El color del carro es", self.color) 
+        print("El precio del carro es $", self.precio)
 
 
-carro = Carro(4, "azul", 5000000)
-print(carro)
+carro = Carro(4, "azul", 50000000)
+carro.datos()
 
-moto = Moto(2, "Verde", 45000000)
-print(moto)
+moto = Moto(2, "Verde", 4500000)
+moto.datos()
+
+
 
 
 
